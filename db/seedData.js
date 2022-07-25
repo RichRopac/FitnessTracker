4af8d5dd413f6d8162faad386825d486969fdb02
TaylorEdelman <Taylor.edelman17@gmail.com>
2022-07-25 12:58:15 -0500

    user error

--- db/seedData.js
+++ db/seedData.js
@@ -39,21 +39,21 @@ async function createTables() {
         id SERIAL PRIMARY KEY,
         name varchar(255) UNIQUE NOT NULL,
         description varchar(255) NOT NULL
-        );
-        CREATE TABLE routines (
-          id SERIAL PRIMARY KEY,
-          "creatorId" INTEGER REFERENCES users(id),
-          "isPublic" BOOLEAN DEFAULT false,
-          goal TEXT NOT NULL
-          );
-          CREATE TABLE routine_activities(
-            id SERIAL PRIMARY KEY,
-            "routineId" INTEGER REFERENCES routines ( id ),
-            "activityId" INTEGER REFERENCES activities ( id ),
-            duration INTEGER,
-            count INTEGER,
-            UNIQUE ("routineId", "activityId")
-          );`);
+      );
+      CREATE TABLE routines (
+        id SERIAL PRIMARY KEY,
+        "creatorId" INTEGER REFERENCES users(id),
+        "isPublic" BOOLEAN DEFAULT false,
+        goal TEXT NOT NULL
+      );
+      CREATE TABLE routine_activities(
+        id SERIAL PRIMARY KEY,
+        "routineId" INTEGER REFERENCES routines ( id ),
+        "activityId" INTEGER REFERENCES activities ( id ),
+        duration INTEGER,
+        count INTEGER,
+        UNIQUE ("routineId", "activityId")
+      );`);
     console.log("Finished Creating Tables!");
   } catch (error) {
     console.error("Error Creating Tables!");
