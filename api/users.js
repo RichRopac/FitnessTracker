b644a4d90f1df3ff66b7c2ce63c12fc04adba2f8
rcropac <rcropac@gmail.com>
2022-07-28 14:21:34 -0500

    Working on Create User api

--- api/users.js
+++ api/users.js
@@ -1,8 +1,35 @@
 /* eslint-disable no-useless-catch */
 const express = require("express");
 const router = express.Router();
+const {createUser, getUser, getUserByUsername} = require("../db/users")
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = process.env
 
 // POST /api/users/register
+
+router.post("/register", async (req, res, next) => {
+    try {
+       const { username, password } = req.body
+       // for end user exists in our database return message
+        if (getUserByUsername(username))
+        {
+        res.send({ message: "Username is Already Taken"})
+        }  
+       // check password length for being too short
+        else if (
+           password.length < 8){
+            res.send({ message: "Password Length is too Short"})
+        } 
+       //another else;
+       else {
+         createUser(username, password)
+       } 
+
+    } catch (error) {
+        console.log(error);
+        next(error); 
+    }
+})
 
 // POST /api/users/login
 
