ee4f2df534a9975a3507aef5af58ac39c5751981
TaylorEdelman <Taylor.edelman17@gmail.com>
2022-07-28 10:10:29 -0500

    api started and server running

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
-require("dotenv").config()
-const express = require("express")
-const app = express()
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const morgan = require("morgan");
+const client = require("./db/client");
+client.connect();
+
+app.use(cors());
 
 // Setup your Middleware and API Router here
+app.use(express.json());
+app.use(morgan("dev"));
+
+const apiRouter = require("./api");
+app.use("/api", apiRouter);
+
+// error handling goes here
 
 module.exports = app;
