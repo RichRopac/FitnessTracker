47c6c867b5661588b7996d2062a470c986595da1
Alia Taha <aliataha5538@gmail.com>
2022-07-29 07:12:04 -0500

    set  api/index

--- api/index.js
+++ api/index.js
@@ -1,8 +1,42 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
+const { getUserById } = require('../db');
+const { JWT_SECRET } = process.env;
+
 const router = express.Router();
 
+// set `req.user` if possible
+router.use(async (req, res, next) => {
+  const prefix = 'Bearer ';
+  const auth = req.header('Authorization');
+
+  if (!auth) {
+    next();
+  } else if (auth.startsWith(prefix)) {
+    const token = auth.slice(prefix.length);
+
+    try {
+      const { id } = jwt.verify(token, JWT_SECRET);
+      if (id) {
+        req.user = await getUserById(id);
+        next();
+      }
+    } catch ({ name, message }) {
+      next({ name, message });
+    }
+  } else {
+    next({
+      name: 'AuthorizationHeaderError',
+      message: `Authorization token must start with ${prefix}`,
+    });
+  }
+});
+
 router.use((req, res, next) => {
-  res.header('Access-Control-Allow-Methods', '*');
+  if (req.user) {
+    console.log('User is set:', req.user);
+  }
+
   next();
 });
 
