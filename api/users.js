09de1a01dd49925d6f681a92509967676fe50874
Alia Taha <aliataha5538@gmail.com>
2022-07-29 10:05:11 -0500

    api/users/register complete

--- api/users.js
+++ api/users.js
@@ -1,35 +1,65 @@
 /* eslint-disable no-useless-catch */
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const {createUser, getUser, getUserByUsername} = require("../db/users")
-const jwt = require("jsonwebtoken");
-const { JWT_SECRET } = process.env
+const { createUser, getUser, getUserByUsername } = require('../db');
+const jwt = require('jsonwebtoken');
+const { requireUser } = require('./utils');
+
+// May not be necessary? a.t.
+router.use((req, res, next) => {
+  console.log('A request is being made to /users');
+
+  next();
+});
 
 // POST /api/users/register
 
-router.post("/register", async (req, res, next) => {
-    try {
-       const { username, password } = req.body
-       // for end user exists in our database return message
-        if (getUserByUsername(username))
-        {
-        res.send({ message: "Username is Already Taken"})
-        }  
-       // check password length for being too short
-        else if (
-           password.length < 8){
-            res.send({ message: "Password Length is too Short"})
-        } 
-       //another else;
-       else {
-         createUser(username, password)
-       } 
+router.post('/register', async (req, res, next) => {
+  const { username, password } = req.body;
 
-    } catch (error) {
-        console.log(error);
-        next(error); 
+  // check password length for being too short
+  if (password.length < 8) {
+    next({
+      name: 'PasswordTooShort',
+      message: 'Password Length is too Short!',
+      error: 'Error with password length..',
+    });
+  }
+
+  // for end user exists in our database return message
+  try {
+    const _user = await getUserByUsername(username);
+
+    if (_user) {
+      next({
+        name: 'UserExistsError',
+        message: 'A user by that username already exists',
+        error: 'Error username already exists..',
+      });
     }
-})
+    //another else;
+    const user = await createUser({
+      username,
+      password,
+    });
+
+    const token = jwt.sign(
+      {
+        id: user.id,
+        username: user.username,
+      },
+      process.env.JWT_SECRET
+    );
+
+    res.send({
+      message: 'Thank you for signing up!',
+      user,
+      token,
+    });
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
+});
 
 // POST /api/users/login
 
