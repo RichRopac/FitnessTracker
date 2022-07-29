6e7ab6bf48f1000a31f15cdeaba65d6984677ba8
Alia Taha <aliataha5538@gmail.com>
2022-07-29 10:25:32 -0500

    api/users/login complete

--- api/users.js
+++ api/users.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 const { createUser, getUser, getUserByUsername } = require('../db');
 const jwt = require('jsonwebtoken');
-const { requireUser } = require('./utils');
+// const { requireUser } = require('./utils');
 
 // May not be necessary? a.t.
 router.use((req, res, next) => {
@@ -62,6 +62,40 @@ router.post('/register', async (req, res, next) => {
 });
 
 // POST /api/users/login
+
+router.post('/login', async (req, res, next) => {
+  const { username, password } = req.body;
+
+  // request must have both
+  if (!username || !password) {
+    next({
+      name: 'MissingCredentialsError',
+      message: 'Please supply both a username and password',
+    });
+  }
+
+  try {
+    const user = await getUser(username, password);
+
+    if (user && user.password == password) {
+      // create token & return to user
+
+      const token = jwt.sign(
+        { id: user.id, username: user.username },
+        process.env.JWT_SECRET
+      );
+      res.send({ message: "You're logged in!", token: token });
+    } else {
+      next({
+        name: 'IncorrectCredentialsError',
+        message: 'Username or password is incorrect',
+      });
+    }
+  } catch (error) {
+    console.error('Error getting user by username, password..');
+    next(error);
+  }
+});
 
 // GET /api/users/me
 
