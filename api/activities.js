22a4a9986c360c72ba719e679c9aa883aa7be36c
rcropac <rcropac@gmail.com>
2022-07-30 12:54:39 -0500

    Saturday Get All Activities

--- api/activities.js
+++ api/activities.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const { getAllActivities } = require("../db");
 
 // GET /api/activities/:activityId/routines
 
 // GET /api/activities
+router.get("/", async (req, res, next) => {
+   
+    try {
+        res.send(await getAllActivities());
+      } catch ({ name, message }) {
+        next({ name, message });
+      }
+    });
 
 // POST /api/activities
 
