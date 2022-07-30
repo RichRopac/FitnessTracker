1babfbd7c22aeb01f6ee44f09fdf424282f40f80
rcropac <rcropac@gmail.com>
2022-07-30 17:33:23 -0500

    Update Activities is Working Now

--- api/activities.js
+++ api/activities.js
@@ -1,43 +1,106 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { getAllActivities, createActivity, getActivityByName } = require("../db");
+const {
+  getAllActivities,
+  createActivity,
+  getActivityByName,
+  getActivityById,
+  updateActivity,
+} = require("../db");
 
 // GET /api/activities/:activityId/routines
 
 // GET /api/activities
 router.get("/", async (req, res, next) => {
-   
-    try {
-        res.send(await getAllActivities());
-      } catch (error) {
-        next(error);
-      }
-    });
+  try {
+    res.send(await getAllActivities());
+  } catch (error) {
+    next(error);
+  }
+});
 
 // POST /api/activities
 router.post("/", async (req, res, next) => {
-  const { name, description } = req.body
+  const { name, description } = req.body;
   // console.log("Req.BODY HERE ", req.body)
   try {
-    const _activity = await getActivityByName(name)
-    // console.log("this is a new activity ", name)
+    const _activity = await getActivityByName(name);
+    // console.log("THIS IS A NEW ACTIVITY", name)
+
     if (_activity) {
       res.send({
         name: "ActivityExistsError",
         message: `An activity with name ${name} already exists`,
         error: "Error Activity already exists..",
       });
-    }else {
-    const activity = await createActivity({ name, description });
-    // console.log ("THIS IS THE NEW ACTIVITY: ", activity )
-    res.send(
-      activity
-    )}
+    } else {
+      const activity = await createActivity({ name, description });
+      // console.log ("THIS IS THE NEW ACTIVITY: ", activity )
+      res.send(activity);
+    }
   } catch (error) {
     next(error);
   }
 });
 
 // PATCH /api/activities/:activityId
+router.patch("/:activityId", async (req, res, next) => {
+  try {
+    const { activityId } = req.params;
+    const { name, description } = req.body;
+    const _activity = await getActivityByName(name);
+    const actID = await getActivityById(activityId);
 
+    console.log("SENT ACTIVITY ID", activityId);
+    console.log("SENT NAME: ", name);
+    console.log("SENT DESCRIPTION: ", description);
+
+    console.log("LOOKED UP ACTIVITY BY NAME: ", _activity);
+    console.log("LOOKED UP ACTIVITY BY ACTIVITY_ID ", actID);
+
+    if (_activity) {
+      res.send({
+        name: "ActivityExistsError",
+        message: `An activity with name ${name} already exists`,
+        error: "Error Activity already exists..",
+      });
+    }
+    console.log(
+      "THIS IS WHAT WE ARE UPDATING: name ",
+      name,
+      " Description ",
+      description
+    );
+
+    if (!actID) {
+      console.log("DOES ACTIVITY EXIST: ", _activity);
+      res.send({
+        name: "ActivityNotFound",
+        message: `Activity ${activityId} not found`,
+        error: "Error Activity does not exist",
+      });
+    }
+    const updateField = {};
+    console.log("UPDATE Field fresh: ", updateField);
+    if (name) {
+      updateField.name = name;
+    }
+    console.log("UPDATE Field name: ", updateField);
+
+    if (description) {
+      updateField.description = description;
+    }
+    console.log("activityId!!!!", activityId);
+    console.log("UPDATE Field description: ", updateField);
+    const updatedActivity = await updateActivity({
+      id: activityId,
+      name: name,
+      description: description,
+    });
+    console.log("THIS IS THE UPDATED ACTIVITY TO SEND BACK: ", updatedActivity);
+    res.send(updatedActivity);
+  } catch (error) {
+    next(error);
+  }
+});
 module.exports = router;
--- db/activities.js
+++ db/activities.js
@@ -1,4 +1,4 @@
-const client = require('./client');
+const client = require("./client");
 
 // database functions
 async function createActivity({ name, description }) {
@@ -17,7 +17,7 @@ async function createActivity({ name, description }) {
 
     return activity;
   } catch (error) {
-    console.error('Error creating activity!');
+    console.error("Error creating activity!");
   }
 }
 
@@ -31,7 +31,7 @@ async function getAllActivities() {
 
     return rows;
   } catch (error) {
-    console.error('Error getting all activities!');
+    console.error("Error getting all activities!");
     throw error;
   }
 }
@@ -48,7 +48,7 @@ async function getActivityById(id) {
     );
     return activity;
   } catch (error) {
-    console.error('Error getting activity by id!');
+    console.error("Error getting activity by id!");
     throw error;
   }
 }
@@ -67,7 +67,7 @@ async function getActivityByName(name) {
     );
     return activity;
   } catch (error) {
-    console.error('Error getting activity by name!');
+    console.error("Error getting activity by name!");
     throw error;
   }
 }
@@ -75,7 +75,7 @@ async function getActivityByName(name) {
 async function attachActivitiesToRoutines(routines) {
   // no side effects
   const routinesToReturn = [...routines];
-  const binds = routines.map((_, index) => `$${index + 1}`).join(', ');
+  const binds = routines.map((_, index) => `$${index + 1}`).join(", ");
   const routineIds = routines.map((routine) => routine.id);
   if (!routineIds?.length) return [];
 
@@ -104,7 +104,7 @@ async function attachActivitiesToRoutines(routines) {
     //console.log('Finished attaching activities to Routines!');
     return routinesToReturn;
   } catch (error) {
-    console.error('Error attaching activities to routines!');
+    console.error("Error attaching activities to routines!");
     throw error;
   }
 }
@@ -117,7 +117,7 @@ async function updateActivity({ id, ...fields }) {
   // build the set string
   const setString = Object.keys(fields)
     .map((key, index) => `"${key}"=$${index + 1}`)
-    .join(', ');
+    .join(", ");
 
   // return early if this is called without fields
   if (setString.length === 0) {
@@ -139,7 +139,7 @@ async function updateActivity({ id, ...fields }) {
 
     return activity;
   } catch (error) {
-    console.error('Error updating acitivity!');
+    console.error("Error updating acitivity!");
     throw error;
   }
 }
